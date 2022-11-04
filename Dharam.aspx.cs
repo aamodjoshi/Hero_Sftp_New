57db0bc7280701289e48b00db33925b0f44293b4
Manoj karunakaran <manoj.karunakaran@bcwebwise1.onmicrosoft.com>
2022-11-04 12:02:45 +0530

    Changes done by dharam on 04-11-2022

---
+++ Dharam.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Hero_Sftp_New
+{
+    public partial class Dharam : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
